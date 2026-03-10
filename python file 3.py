a6f37c85095cb394b027cbfa9eb78a57290db034
Nandhini70 <nandhinianbu203@gmail.com>
2026-03-10 13:38:45 +0530

    Add files via upload

---
+++ python file 3.py
@@ -0,0 +1,85 @@
+'''a=10
+b=5
+print(a)'''
+'''a=10
+b=5
+print(b)'''
+'''a=10
+b=5
+print(a*b)'''
+'''#complement:
+a=10
+print(~a)'''
+'''n=-45
+print(~n)'''
+'''a=7
+print(a<<2)'''
+#rightshift
+'''a=137
+print(a>>3)'''
+#logical oprator:
+'''a=10
+b=20
+c=10
+d=45
+print(a<b and b>=c)
+print(a<d and d<c)
+print(a<d or d<c)'''
+#if else
+'''length=int(input("enter length:"))
+breath=int(input("enter breath:"))
+if length == breath:
+    print("square")
+else:print("rectangle")'''
+'''# squrae below or equal:
+num=int(input("enter a number:"))
+square=int(input("enter a number:"))
+if square>50:
+    print("square is above50")
+else:
+    print("square is below or equal")'''
+'''#question 3
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+diff=a-b
+if diff ==0:
+    print("difference is 0")
+else:
+    print("difference is not 0")'''
+'''#question 4
+marks=int(input("enter marks:"))
+if marks >=50:
+    print("passed")
+else:
+    print("fail")'''
+'''#question 5
+num=int(input("enter number"))
+if num%10==0:
+   print("divisible")
+else:
+    print("not divisible")'''
+'''#question 6
+a=int(input("enter numbr:"))
+b=int(input("enter number:"))
+if a>b:
+    print("a is big")
+else:
+    print(" b is above")'''
+#question 7
+a=int(input("enter value:"))
+if a==1:
+    print("you can go out and play")
+else:
+    print("you cannot go out and play")
+
+    
+
+
+
+
+    
+
+
+
+
+
